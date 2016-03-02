e50595acde4ae9d0e67b028c90beff0c49f0a05b
ShevT <shev_t@mail.ru>
2016-03-02 15:02:58 +0300

    initial common

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,132 @@
+#
+# Copyright (C) 2016 ShevT
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+COMMON_PATH := device/huawei/omap4-common
+
+# Camera
+CAMERAHAL_HUAWEI_OMAP4 := true
+COMMON_GLOBAL_CFLAGS += -DCAMERAHAL_HUAWEI_OMAP4
+TI_CAMERAHAL_USES_LEGACY_DOMX_DCC := true
+TI_CAMERAHAL_MAX_CAMERAS_SUPPORTED := 2
+#TI_CAMERAHAL_DEBUG_ENABLED := true
+
+# OMX buffer reallocate
+BOARD_CANT_REALLOCATE_OMX_BUFFERS := true
+
+# Apply the compass filter
+BOARD_INVENSENSE_APPLY_COMPASS_NOISE_FILTER := true
+
+# Platform
+TARGET_BOARD_OMAP_CPU := 4460
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+ARCH_ARM_HAVE_NEON := true
+ARCH_ARM_HAVE_VFP := true
+
+# Kernel
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_PAGESIZE := 2048
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/../../linaro-4.9/bin/
+#BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+
+# For RIL
+TARGET_NEEDS_BIONIC_MD5 := true
+
+# Webkit
+ENABLE_WEBGL := true
+
+# Audio
+BOARD_USES_GENERIC_AUDIO := false
+USE_LEGACY_AUDIO_POLICY := 1
+CONVERT_AUDIO_DEVICES := true
+BUILD_AUDIO_HW_WRAPPER := true
+AUDIO_FEATURE_DEEP_BUFFER_RINGTONE := true
+
+# Graphics
+BOARD_EGL_CFG := $(COMMON_PATH)/configs/egl/egl.cfg
+USE_OPENGL_RENDERER := true
+# As in the kernel (2)
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 2
+
+BOARD_USE_TI_DUCATI_H264_PROFILE := true
+
+# set if the target supports FBIO_WAITFORVSYNC
+TARGET_HAS_WAITFORVSYNC := true
+
+# External SGX Module
+SGX_MODULES:
+	make clean -C $(HARDWARE_TI_OMAP4_BASE)/pvr-source/eurasiacon/build/linux2/omap4430_android
+	cp $(TARGET_KERNEL_SOURCE)/drivers/video/omap2/omapfb/omapfb.h $(KERNEL_OUT)/drivers/video/omap2/omapfb/omapfb.h
+	make -j8 -C $(HARDWARE_TI_OMAP4_BASE)/pvr-source/eurasiacon/build/linux2/omap4430_android ARCH=arm KERNEL_CROSS_COMPILE=arm-eabi- CROSS_COMPILE=arm-eabi- KERNELDIR=$(KERNEL_OUT) TARGET_PRODUCT="blaze_tablet" BUILD=release TARGET_SGX=540
+	mv $(KERNEL_OUT)/../../target/kbuild/pvrsrvkm_sgx540_120.ko $(KERNEL_MODULES_OUT)
+	$(ARM_EABI_TOOLCHAIN)/arm-eabi-strip --strip-unneeded $(KERNEL_MODULES_OUT)/pvrsrvkm_sgx540_120.ko
+
+TARGET_KERNEL_MODULES += SGX_MODULES
+
+# Use dlmalloc
+MALLOC_IMPL := dlmalloc
+
+# Lights
+TARGET_PROVIDES_LIBLIGHTS := true
+
+# Wifi related defines
+BOARD_WLAN_DEVICE                := bcmdhd
+WIFI_BAND                        := 802_11_ABG
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
+WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA          := "/vendor/firmware/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_AP           := "/vendor/firmware/fw_bcmdhd_apsta.bin"
+
+# Enable dex-preoptimization to speed up first boot sequence
+WITH_DEXPREOPT := true
+WITH_DEXPREOPT_BOOT_IMG_ONLY := true
+
+# Include an expanded selection of fonts
+EXTENDED_FONT_FOOTPRINT := true
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+
+# Partitions
+BOARD_FLASH_BLOCK_SIZE := 4096
+
+# FM
+BOARD_HAVE_FM_RADIO := true
+BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
+BUILD_FM_RADIO := true
+
+# Vold
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/f_mass_storage/lun/file"
+
+# Charger/Healthd
+BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_CHARGER_SHOW_PERCENTAGE := true
+
+# File System
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+
+# Disable journaling on system.img to save space
+BOARD_SYSTEMIMAGE_JOURNAL_SIZE := 0
+
+# Recovery
+TARGET_PREBUILT_RECOVERY_KERNEL := $(COMMON_PATH)/recovery/recovery_kernel
---
+++ device-common.mk
@@ -0,0 +1,165 @@
+#
+# Copyright (C) 2016 ShevT
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVCOMMON_PATH := device/huawei/omap4-common
+
+PRODUCT_CHARACTERISTICS := default
+
+# Audio Packages
+PRODUCT_PACKAGES += \
+    tinycap \
+    tinymix \
+    tinyplay \
+    libtinyalsa \
+    libaudioutils
+
+# Hardware HALs
+PRODUCT_PACKAGES += \
+    audio.a2dp.default \
+    audio.usb.default \
+    audio.r_submix.default \
+    audio.primary.omap4 \
+    gralloc.omap4.so \
+    camera.omap4 \
+    memtrack.omap4 \
+    power.front \
+    lights.omap4
+
+# Replace Camera2 to Snap
+PRODUCT_PACKAGES += \
+    Snap
+
+# init.d
+PRODUCT_COPY_FILES += \
+    $(DEVCOMMON_PATH)/utilities/init.d/11frandom:system/etc/init.d/11frandom
+
+# Media / Audio
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    $(DEVCOMMON_PATH)/configs/media/media_profiles.xml:system/etc/media_profiles.xml \
+    $(DEVCOMMON_PATH)/configs/media/media_codecs.xml:system/etc/media_codecs.xml \
+    $(DEVCOMMON_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf
+
+# WiFi
+PRODUCT_PACKAGES += \
+    dhcpcd.conf \
+    hostapd \
+    hostapd_default.conf \
+    libwpa_client \
+    wpa_supplicant \
+    wpa_supplicant.conf
+
+# Common
+PRODUCT_PACKAGES += \
+    make_ext4fs \
+    setup_fs \
+    e2fsck \
+    com.android.future.usb.accessory
+
+# Utilities
+PRODUCT_PACKAGES += \
+    remount \
+    optimizedb \
+    optimizestorage
+
+# Network tools
+PRODUCT_PACKAGES += \
+    libpcap \
+    tcpdump
+
+# IPv6 tethering
+PRODUCT_PACKAGES += \
+    ebtables \
+    ethertypes
+
+# Charging mode
+PRODUCT_PACKAGES += \
+    charger_res_images
+
+# Key maps
+PRODUCT_COPY_FILES += \
+    $(DEVCOMMON_PATH)/configs/usr/omap4-keypad.kl:system/usr/keylayout/omap4-keypad.kl \
+    $(DEVCOMMON_PATH)/configs/usr/omap4-keypad.kcm:system/usr/keychars/omap4-keypad.kcm \
+    $(DEVCOMMON_PATH)/configs/usr/twl6030_pwrbutton.kl:system/usr/keylayout/twl6030_pwrbutton.kl
+
+# Input device calibration files
+PRODUCT_COPY_FILES += \
+    $(DEVCOMMON_PATH)/configs/usr/syn_tm12xx_ts_1.idc:system/usr/idc/syn_tm12xx_ts_1.idc \
+    $(DEVCOMMON_PATH)/configs/usr/syn_tm12xx_ts_2.idc:system/usr/idc/syn_tm12xx_ts_2.idc
+
+# These are the hardware-specific features
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/native/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml
+
+ADDITIONAL_DEFAULT_PROPERTIES += \
+    ro.allow.mock.location=1 \
+    persist.sys.usb.config=mtp
+
+# Newer camera API isn't supported.
+PRODUCT_PROPERTY_OVERRIDES += \
+    camera2.portability.force_api=1
+
+# Disable VFR support for encoders
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.vfr.enable=0
+
+# Avoids retrying for an EGL config w/o EGL_SWAP_BEHAVIOR_PRESERVED
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.hwui.render_dirty_regions=false
+
+# SGX540 is slower with the scissor optimization enabled
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hwui.disable_scissor_opt=true
+
+# I/O Scheduler
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.io.scheduler=bfq
+
+# Low-RAM optimizations
+ADDITIONAL_BUILD_PROPERTIES += \
+    ro.config.low_ram=true \
+    dalvik.vm.jit.codecachesize=0 \
+    persist.sys.force_highendgfx=true \
+    config.disable_atlas=true \
+    dalvik.vm.dex2oat-flags=--no-watch-dog \
+    ro.sys.fw.dex2oat_thread_count=2
+
+ADDITIONAL_BUILD_PROPERTIES += \
+    ro.com.android.dataroaming=false
+
+# Disabling strict mode
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.strictmode.visual=0 \
+    persist.sys.strictmode.disable=1
+
+$(call inherit-product, hardware/broadcom/wlan/bcmdhd/firmware/bcm4330/device-bcm.mk)
