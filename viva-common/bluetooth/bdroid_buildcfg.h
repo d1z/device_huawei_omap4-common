751bb010b9955e2c263767ea7ea688191ab3b0ad
diz <diz@ubuntu.local>
2016-03-13 06:53:17 -0700

    Change bluetooth device name

--- viva-common/bluetooth/bdroid_buildcfg.h
+++ viva-common/bluetooth/bdroid_buildcfg.h
@@ -17,7 +17,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Huawei U9500"
+#define BTM_DEF_LOCAL_NAME   "Huawei U9200"
 
 #define BLE_INCLUDED TRUE
 #define BTA_GATT_INCLUDED TRUE
